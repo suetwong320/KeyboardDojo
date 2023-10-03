0d3cb33c7e024880181756daf0339d8dafa9c009
Leo <82315240+leohonlmc@users.noreply.github.com>
2023-10-02 23:44:03 -0400

    UI update

--- src/components/partial/Footer.jsx
+++ src/components/partial/Footer.jsx
@@ -4,33 +4,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Footer() {
   return (
-    <section className="">
-      <footer className="text-center text-white">
-        <div className="container p-4 pb-0">
-          {/* <section className="">
-            <p className="d-flex justify-content-center align-items-center">
-              <span className="me-3">Register for free</span>
-              <button
-                type="button"
-                className="btn btn-outline-light btn-rounded"
-              >
-                Sign up!
-              </button>
-            </p>
-          </section> */}
-        </div>
-
-        <div
-          className="text-center p-3"
-          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-        >
-          © 2023 Copyright:{" "}
-          <a className="text-white" href="https://keyboarddojo.onrender.com/">
-            keyboardDojo.com
-          </a>
-        </div>
-      </footer>
-    </section>
+    <footer className="text-center text-white Footer">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2023 Copyright:{" "}
+        <a className="text-white" href="https://keyboarddojo.onrender.com/">
+          keyboardDojo.com
+        </a>
+      </div>
+    </footer>
   );
 }
 
