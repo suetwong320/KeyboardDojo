fd42eb8c261c62dae906018e8022b929c596ba1d
Leo <82315240+leohonlmc@users.noreply.github.com>
2023-10-03 15:03:01 -0400

    fixed wpm

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import React, { useEffect, useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import Footer from "./partial/Footer";
-
 import Result from "./popup/Result";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKeyboard } from "@fortawesome/free-solid-svg-icons";
+
 function Home() {
-  const paragraphs = [
+  const paragraphsEnglish = [
     "The wise owl perched atop the ancient oak, its eyes glowing like golden orbs in the moonlight, watched silently as the nocturnal creatures emerged from their hidden realms. The silent whispers of the leaves told tales of unseen worlds, weaving a symphony of secrets untold.",
     "In a realm where dragons soared above, their scales shimmering in the sun's embrace, a lone knight ventured into the unknown, his armor clinking softly. The air was ripe with the fragrance of the myriad of flowers that danced in the gentle breeze, whispering secrets of valor and magic.",
     "The endless cascade of binary data flowed like a digital river through the labyrinth of circuits and transistors. The computer hummed softly, its artificial intelligence contemplating the vastness of human experience and the myriad mysteries of the universe, unbounded by time.",
@@ -30,7 +32,53 @@ function Home() {
     "The eternal dance of the celestial bodies painted the canvas of the cosmos with the hues of time and space. The whispers of the galaxies unfolded in a symphony of light and darkness, each twinkling star a note in the song of the universe.",
   ];
 
-  const sentences = [
+  const paragraphsFrench = [
+    "Le soleil se couchait à l'horizon, colorant le ciel de teintes orangées et pourpres. Les oiseaux retournaient à leurs nids, chantant des mélodies douces.",
+    "Dans le petit village au bord de la mer, les pêcheurs rangeaient leurs filets, préparant leurs bateaux pour le lendemain. La brise salée caressait leurs visages fatigués.",
+    "Juliette marchait le long du boulevard, les lumières de la ville brillant autour d'elle. Chaque pas racontait une histoire, chaque coin de rue évoquait un souvenir.",
+    "Le château, perché sur la colline, dominait le paysage. Ses tours anciennes et ses murs en pierre racontaient des histoires d'amour, de trahison et de bravoure.",
+    "La forêt était un sanctuaire de paix et de tranquillité. Les arbres s'élevaient fièrement, leurs feuilles murmurant les secrets de la nature.",
+    "Au cœur de Paris, la Seine serpentait avec grâce, reflétant les lumières de la ville. Les amoureux marchaient le long des quais, main dans la main.",
+    "L'odeur du pain frais s'échappait de la boulangerie, attirant les passants. Chaque croissant, chaque baguette racontait l'histoire du boulanger et de son art.",
+    "Dans le musée, les tableaux racontaient l'histoire de générations d'artistes. Les couleurs, les formes et les textures évoquaient des émotions intenses.",
+    "La montagne se dressait majestueusement, couverte de neige. Les skieurs descendaient les pistes avec enthousiasme, laissant derrière eux des traces éphémères.",
+    "La pluie tombait doucement sur les toits de la ville. Les gouttes d'eau jouaient une mélodie apaisante, berçant les rêves de ceux qui écoutaient.",
+    "Chaque fleur dans le jardin avait une histoire à raconter. Les roses rouges parlaient d'amour passionné, tandis que les marguerites évoquaient l'innocence.",
+    "Les enfants couraient dans le parc, leurs rires résonnant dans l'air frais du printemps. Le carrousel tournait sans cesse, portant avec lui des rêves de jeunesse.",
+    "Les étoiles scintillaient dans le ciel nocturne, chacune d'elles étant une énigme de l'univers. Les astronomes les observaient avec curiosité, cherchant à percer leurs secrets.",
+    "Le marché était animé de conversations et de marchandage. Les étals débordaient de fruits, de légumes et de spécialités locales.",
+    "Le vin vieillissait doucement dans la cave, attendait patiemment le moment où il serait dégusté. Chaque bouteille racontait l'histoire de la vigne et du vigneron.",
+    "Le chat se prélassait au soleil, profitant de chaque rayon. Il étirait ses pattes et ronronnait de contentement, sans souci du monde.",
+    "Le festival remplissait les rues de musique, de danse et de couleur. Les gens célébraient, oubliant leurs soucis et se laissant emporter par la joie.",
+    "Le moulin à vent se dressait fièrement dans le champ, ses ailes tournant au gré du vent. Il était le gardien des récoltes et des saisons.",
+    "La bibliothèque était un trésor de connaissances. Chaque livre était une porte vers un autre monde, une invitation à l'aventure et à la découverte.",
+    "L'automne transformait le paysage en une palette de couleurs vives. Les feuilles tombaient en tourbillonnant, annonçant l'arrivée du froid hivernal.",
+  ];
+
+  const paragraphsSpanish = [
+    "Durante mis vacaciones, visité el pequeño pueblo de Ronda en España. Las vistas desde el puente eran simplemente impresionantes y la historia del lugar me cautivó.",
+    "Los mercados en México están llenos de colores vibrantes. Puedes encontrar todo, desde frutas frescas hasta tejidos tradicionales, y el ambiente es simplemente electrizante.",
+    "En la tranquilidad de la sierra, hay un pequeño refugio donde los viajeros pueden desconectar del mundo y disfrutar de la naturaleza en su estado más puro.",
+    "El flamenco es mucho más que un simple baile; es una expresión profunda del alma andaluza. Ver una actuación en vivo puede ser una experiencia verdaderamente conmovedora.",
+    "Cada vez que miro el horizonte desde la costa, me pierdo en el inmenso azul del océano. Es un recordatorio constante de lo vasto y misterioso que es nuestro mundo.",
+    "Barcelona es una mezcla fascinante de lo antiguo y lo nuevo. Mientras paseas por sus calles, es posible que te encuentres con una iglesia gótica justo al lado de un rascacielos moderno.",
+    "Mi abuela solía contarme historias de su juventud, de tiempos más simples, donde la vida giraba en torno a la familia, el trabajo y la comunidad.",
+    "Durante las fiestas, las calles de Madrid se llenan de vida. Las luces brillan por todas partes, y la música y la risa resuenan en cada esquina.",
+    "Hay un pequeño café en el corazón de Buenos Aires donde los poetas y músicos se reúnen para compartir sus obras. Es un rincón mágico que parece detenido en el tiempo.",
+    "El río Amazonas es una maravilla de la naturaleza. Viajar a lo largo de sus aguas te brinda una perspectiva completamente diferente de la vida y el medio ambiente.",
+    "El tango es el alma de Argentina. Es más que un baile; es una historia de pasión, amor y nostalgia.",
+    "Caminar por las calles empedradas de Antigua, Guatemala, es como viajar atrás en el tiempo. Cada edificio y cada callejón tienen su propia historia que contar.",
+    "Los atardeceres en el Caribe son algo que nunca olvidarás. El cielo se tiñe de tonos anaranjados y rojos, mientras el sol se hunde lentamente en el mar.",
+    "El arte de tejer ha sido pasado de generación en generación en los Andes. Las técnicas tradicionales han sido preservadas y todavía se pueden ver en los mercados locales.",
+    "La comida mexicana es una explosión de sabores y texturas. Desde los picantes tacos hasta el dulce churro, hay algo para satisfacer a todos los paladares.",
+    "La ciudad de Machu Picchu, perdida en lo alto de las montañas, es un testimonio del ingenio y la perseverancia del pueblo inca.",
+    "Durante el Día de los Muertos en México, las familias se reúnen para recordar y honrar a sus seres queridos que han fallecido. Es una celebración llena de color, música y amor.",
+    "La selva del Yucatán esconde muchos secretos. Entre ellos, antiguas ciudades mayas que una vez fueron el epicentro de una civilización floreciente.",
+    "El arte callejero en Valparaíso, Chile, es una manifestación de la creatividad y la resistencia del pueblo. Cada mural cuenta una historia y refleja la identidad de la comunidad.",
+    "La Patagonia es un lugar de belleza salvaje y prístina. Desde sus imponentes montañas hasta sus vastos lagos, es un paraíso para los amantes de la naturaleza.",
+  ];
+
+  const sentencesEnglish = [
     "The moonlight danced on the tranquil sea.",
     "Leaves whispered secrets in a gentle breeze.",
     "The clock ticked away, echoing in silence.",
@@ -53,12 +101,58 @@ function Home() {
     "The flowers bloomed in a rainbow of colors.",
   ];
 
+  const sentencesFrench = [
+    "Les oiseaux chantent dès l'aube.",
+    "Marie prépare des crêpes pour le petit déjeuner.",
+    "La Seine traverse la belle ville de Paris.",
+    "Chaque hiver, nous allons skier dans les Alpes.",
+    "Les chiens sont les meilleurs amis de l'homme.",
+    "J'adore lire des romans au clair de lune.",
+    "La musique apporte la joie dans nos cœurs.",
+    "Les vendredis soir, nous allons souvent au cinéma.",
+    "La vie est pleine de surprises inattendues.",
+    "La tour Eiffel est le symbole de Paris.",
+    "Les enfants jouent dans la cour pendant la récréation.",
+    "Les raisins mûrissent sous le soleil d'été.",
+    "Elle adore voyager et découvrir de nouvelles cultures.",
+    "Il est important de toujours dire la vérité.",
+    "La beauté de la nature m'inspire tous les jours.",
+    "Le chocolat est ma gourmandise préférée.",
+    "La famille est le pilier de la société.",
+    "La poésie exprime les émotions de l'âme.",
+    "J'aimerais visiter la Provence au printemps.",
+    "Le football est le sport le plus populaire en France.",
+  ];
+
+  const sentencesSpanish = [
+    "Los pájaros cantan al amanecer.",
+    "María prepara tortitas para el desayuno.",
+    "El río Sena atraviesa la hermosa ciudad de París.",
+    "Cada invierno, vamos a esquiar en los Alpes.",
+    "Los perros son los mejores amigos del hombre.",
+    "Me encanta leer novelas a la luz de la luna.",
+    "La música trae alegría a nuestros corazones.",
+    "Los viernes por la noche, solemos ir al cine.",
+    "La vida está llena de sorpresas inesperadas.",
+    "La Torre Eiffel es el símbolo de París.",
+    "Los niños juegan en el patio durante el recreo.",
+    "Las uvas maduran bajo el sol del verano.",
+    "A ella le encanta viajar y descubrir nuevas culturas.",
+    "Es importante decir siempre la verdad.",
+    "La belleza de la naturaleza me inspira todos los días.",
+    "El chocolate es mi golosina favorita.",
+    "La familia es el pilar de la sociedad.",
+    "La poesía expresa las emociones del alma.",
+    "Me gustaría visitar la Provenza en primavera.",
+    "El fútbol es el deporte más popular en España.",
+  ];
+
   const [correct, setCorrect] = React.useState(0);
   const [incorrect, setIncorrect] = React.useState(0);
   const [incorrectIndices, setIncorrectIndices] = useState([]);
   const [index, setIndex] = React.useState(0);
   const [placeholder, setPlaceholder] = React.useState(
-    "Click here to start typing..."
+    `Click here to start typing...`
   );
 
   const [width, setWidth] = useState(window.innerWidth);
@@ -73,7 +167,7 @@ function Home() {
 
   const activeSpan = useRef(null);
 
-  const [text, setText] = useState(paragraphs);
+  const [text, setText] = useState(paragraphsEnglish);
 
   const [elapsedTime, setElapsedTime] = useState(0);
 
@@ -83,9 +177,9 @@ function Home() {
 
   useEffect(() => {
     if (width < 767) {
-      setText(sentences);
+      setText(sentencesEnglish);
     } else {
-      setText(paragraphs);
+      setText(paragraphsEnglish);
     }
   }, [width]);
 
@@ -103,19 +197,6 @@ function Home() {
       });
     }
   }, [index]);
-
-  // useEffect(() => {
-  //   const handleBlur = () => {
-  //     inputRef.current.focus();
-  //   };
-
-  //   const currentInput = inputRef.current;
-  //   currentInput.addEventListener("blur", handleBlur);
-
-  //   return () => {
-  //     currentInput.removeEventListener("blur", handleBlur);
-  //   };
-  // }, []);
 
   useEffect(() => {
     let interval;
@@ -197,7 +278,7 @@ function Home() {
           correct={correct}
           incorrect={incorrect}
           accuracy={formatAsPercentage((correct / randText.length) * 100)}
-          wpm={~~(text.length / (elapsedTime / 60000))}
+          wpm={~~(text.length / 5 / (elapsedTime / 60000))}
         />
       )}
 
@@ -226,6 +307,7 @@ function Home() {
             ref={inputRef}
             placeholder={placeholder}
           />
+          <FontAwesomeIcon icon={faKeyboard} className="eye-icon" />
         </div>
       </div>
 
